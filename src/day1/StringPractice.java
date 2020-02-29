1b3eebb78d769b654ea70e369eb6a2dbf957ec7b
Seyfullah Yavuz <seyfullahtx@gmail.com>
2020-02-29 14:50:20 -0600

    First Git project

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -1,8 +1,10 @@
 package day1;
 public class StringPractice {
 
-    public static void main (String []args){
+    public static void main (String []args)
+    {
         System.out.println("Hello world");
+        System.out.println("My first Git Practice");
     }
 
 }
